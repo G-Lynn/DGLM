c72370ec854c12be14f47df87ba23f5c72790d42
Chris Glynn <christopher.glynn@unh.edu>
2017-02-07 19:09:00 -0800

    updated directory

--- Code/main.R
+++ Code/main.R
@@ -5,7 +5,7 @@ library(BayesLogit)
 library(Rcpp)
 dir = "~/sDGLM-master/"
 
-load(paste(dir,"Data/Data_MCMC.RData",sep=""))
+load(paste(dir,"Data/MCMC_Data.RData",sep=""))
 source(paste(dir,"Code/Theta_FFBS_Collapsed.R",sep=""))
 source(paste(dir,"Code/Gamma_Zeta_FFBS_Collapsed.R",sep=""))
 #source(paste(dir,"Code/Omega_Step_Collapsed.R",sep=""))
@@ -26,15 +26,15 @@ for(t in 1:t.T) Kappa[[t]] = y[[t]] - .5*N[[t]]
 Post.Mean.Theta = Post.Err.Theta = list()
 Post.Mean.Gamma = Post.Err.Gamma = list()
 
-B = 1000 #1000 #20000
+B = 10 #1000 #20000
 Thin.Rate = 1 #10
-nSamples = 1000 #1000 #10000
+nSamples = 10 #1000 #10000
 INIT_base = 0
 inits = INIT_base + 1 
 print(inits)
 
 N.MC = B+Thin.Rate*nSamples
-nCores = 8
+nCores = 1
 sigma2.init = 2*diag(W)
 ptm = proc.time()[3]
 mclapply(inits, function(init) MCMC.collapsed(init,dir,nSamples,N.MC,Thin.Rate,m0,C0,W,sigma2.theta = sigma2.init,t.T,n,K,PI.G_Z.0,Q.gamma_zeta,N,y), mc.cores = nCores )
